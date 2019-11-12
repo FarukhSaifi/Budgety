31a3382d800073f810e83a5ee7ca6e5d46f6d7c8
Farukh1x95 <farook1x95@gmail.com>
2019-11-13 02:46:16 +0530

    UI Improvement

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="container d-flex justify-content-center py-4 text-center">
-        <div className="col-4">
+        <div className="col-12 col-md-6 ">
           <p className="lead">Available Bugdet </p>
           <h1 className="display-4">+ 16699 </h1>
           <div className="income-total rounded p-3 mb-2">
