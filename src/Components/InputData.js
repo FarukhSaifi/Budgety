7a3b2e29cf0efc910e08b57f8fe4ceefe7b427a8
Farukh1x95 <farook1x95@gmail.com>
2019-11-14 01:50:44 +0530

    Bugdet Module Working

--- src/Components/InputData.js
+++ src/Components/InputData.js
@@ -6,60 +6,99 @@ class InputData extends Component {
     records: [
       {
         id: 1,
-        dollar: 100,
-        type: "inc",
-        note: "gift"
+        dollar: "100",
+        mType: "inc",
+        note: "black money"
       },
       {
         id: 2,
         dollar: 345,
-        type: "inc",
+        mType: "inc",
         note: "fun time"
       },
       {
         id: 3,
         dollar: 250,
-        type: "exp",
+        mType: "exp",
         note: "pocket money"
       },
       {
         id: 4,
         dollar: 550,
-        type: "exp",
+        mType: "exp",
         note: "bang money"
       }
-    ]
+    ],
+
+    dollar: "",
+    mType: "",
+    note: ""
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    const records = {
+      dollar: this.state.dollar,
+      mType: this.state.mType,
+      note: this.state.note
+    };
+    // console.log(test);
+
+    this.setState({
+      records: [...this.state.records, records]
+    });
+    // console.log(records);
+    console.log(this.state);
   };
 
   render() {
+    // const { id, dollar, mType, note } = this.state;
     const { records } = this.state;
     return (
       <div>
-        <div className="d-flex justify-content-center align-items-center p-2 my-2  text-dark-50 bg-light rounded shadow-sm">
-          <div className="form-group">
-            <select className="form-control" name="Type">
-              <option value={"inc"}>+</option>
-              <option value={"exp"}>-</option>
-            </select>
-          </div>
-          <div className="form-group">
-            <input
-              className="add__description"
-              type="text"
-              name="value"
-              placeholder="Description"
-              value={this.note}
-            />
-          </div>
-          <div className="form-group">
-            <input type="number" className="add__value" placeholder="Value" />
-          </div>
+        <div className="d-flex justify-content-center align-items-center p-2 my-2 text-dark-50 bg-light rounded shadow-sm">
+          <form onSubmit={this.onSubmit} key={records.id} className="d-flex">
+            <div className="form-group">
+              <select
+                value={records.mType}
+                className="form-control"
+                name="mType"
+                onChange={this.onChange}
+              >
+                <option value="N/A">N/A</option>
+                <option value={"inc"}>+</option>
+                <option value={"exp"}>-</option>
+              </select>
+            </div>
+            <div className="form-group">
+              <input
+                className="add__description"
+                type="text"
+                name="note"
+                placeholder="Description"
+                value={records.note}
+                onChange={this.onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="number"
+                name="dollar"
+                value={this.state.records.dollar}
+                className="add__value"
+                placeholder="Value"
+                onChange={this.onChange}
+              />
+            </div>
 
-          <button className="add__btn">
-            <i className="ion-ios-checkmark-outline"></i>
-          </button>
+            <button className="add__btn">
+              <i className="ion-ios-checkmark-outline"></i>
+            </button>
+          </form>
         </div>
-        {console.log(records)}
+        {/* {console.log(records)} */}
         <Budget records={records} />
       </div>
     );
