cc11c3e50f8ab14579bd27e4793cce255a4805ad
Farukh1x95 <farook1x95@gmail.com>
2019-11-18 02:12:06 +0530

    Add Context APi

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import React from "react";
 import "./App.css";
+import Provider from "./Components/Database";
+
 import InputData from "./Components/InputData";
+import BudgetHeader from "./Components/BudgetHeader";
+import Budget from "./Components/Budget";
 
 function App() {
   return (
-    <div className="App">
-      <header className="container d-flex justify-content-center py-4 text-center">
-        <div className="col-12 col-md-6 ">
-          <p className="lead">Available Bugdet </p>
-          <h1 className="display-4">+ 16699 </h1>
-          <div className="income-total rounded p-3 mb-2">
-            <p className="text-uppercase mb-0 text-left">
-              Income: <span className="float-right">1234 </span>
-            </p>
-          </div>
-          <div className="exp-total rounded p-3">
-            <p className="text-uppercase mb-0 text-left">
-              Expensive: <span className="float-right">1234 </span>
-            </p>
-          </div>
-        </div>
-      </header>
-      <InputData />
-    </div>
+    <Provider>
+      <div className="App">
+        <BudgetHeader />
+        <InputData />
+        <Budget />
+      </div>
+    </Provider>
   );
 }
 
